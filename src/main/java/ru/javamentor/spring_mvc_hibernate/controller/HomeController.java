90aa2be78d770b6747e80de28872e190e007116c
mymindinsane <arturguletskiy21@gmail.com>
2025-02-20 11:00:46 +0300

    feat: add HomeController

---
+++ src/main/java/ru/javamentor/spring_mvc_hibernate/controller/HomeController.java
@@ -0,0 +1,12 @@
+package ru.javamentor.spring_mvc_hibernate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+}
